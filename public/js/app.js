e719928fdd8db98c10d009ade80ef3d796b67b0e
Daniel Costeira <costeiradaniel@gmail.com>
2020-02-09 02:14:25 +0000

    add abou text

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,8 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = search.value;
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
 
   fetch("/weather?address=" + location).then((response) => {
     response.json().then((data) => {
