3eb3904f5154d9aadb53922afd31322c19f06674
Daniel Costeira <costeiradaniel@gmail.com>
2020-02-09 02:06:03 +0000

    Layout changes to show the forecast

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,10 +11,10 @@ weatherForm.addEventListener("submit", (e) => {
   fetch("/weather?address=" + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        console.log(data.error)
+        messageOne.textContent = data.error;
       } else {
-        console.log(data.location);
-        console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
       }
     });
   });
